c8fa6ca4b9fd82e006dc6d85df82f903924eef35
Unknown <thibault.fiacre@gmail.com>
2018-01-08 17:38:24 +0100

    Modification de gitignore pour ajout de vendor/friendsofsymfony

---
+++ vendor/friendsofsymfony/user-bundle/Form/Type/ResettingFormType.php
@@ -0,0 +1,80 @@
+<?php
+
+/*
+ * This file is part of the FOSUserBundle package.
+ *
+ * (c) FriendsOfSymfony <http://friendsofsymfony.github.com/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace FOS\UserBundle\Form\Type;
+
+use FOS\UserBundle\Util\LegacyFormHelper;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ResettingFormType extends AbstractType
+{
+    /**
+     * @var string
+     */
+    private $class;
+
+    /**
+     * @param string $class The User class name
+     */
+    public function __construct($class)
+    {
+        $this->class = $class;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('plainPassword', 
+            LegacyFormHelper::getType('Symfony\Component\Form\Extension\Core\Type\RepeatedType'), 
+            array(
+                'type' => LegacyFormHelper::getType('Symfony\Component\Form\Extension\Core\Type\PasswordType'),
+                'options' => array('translation_domain' => 'FOSUserBundle'),
+                'first_options' => array('label' => 'Nouveau mot de passe'),
+                'second_options' => array('label' => 'Confirmation nouveau mot de passe'),
+                'invalid_message' => 'Les deux mot de passe ne sont pas identiques.',
+                'attr' => array('class' => 'form-control col-lg-10 m-auto')
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => $this->class,
+            'csrf_token_id' => 'resetting',
+            // BC for SF < 2.8
+            'intention' => 'resetting',
+        ));
+    }
+
+    // BC for SF < 3.0
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return $this->getBlockPrefix();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'fos_user_resetting';
+    }
+}
