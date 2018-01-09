6173229ec9277aedbd05f0485014646aa3f0be1b
Unknown <thibault.fiacre@gmail.com>
2018-01-09 11:48:35 +0100

    Suppression d'un FOSUserBundle en trop dans le Kernel

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -9,7 +9,6 @@ class AppKernel extends Kernel
     public function registerBundles()
     {
         $bundles = [
-            new FOS\UserBundle\FOSUserBundle(),
             new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
             new Symfony\Bundle\SecurityBundle\SecurityBundle(),
             new Symfony\Bundle\TwigBundle\TwigBundle(),
