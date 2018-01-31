abbf24861b3d56f0b5485f6870251074e7c3ab03
Unknown <thibault.fiacre@gmail.com>
2018-01-31 14:13:58 +0100

    Auto stash before merge of "map" and "origin/map"

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -27,29 +27,22 @@ class DefaultController extends Controller
         $observations = $this->getDoctrine()->getRepository('AppBundle:Observations')->getAllObservations();
 
         return $this->render('default/map.html.twig', array(
-
             'birds'        => $birds,
             'observations' => $observations,
-
         ));
-
     }
 
     public function raceSelectAction(Request $request) {
 
         if($request->isXmlHttpRequest() && $request->isMethod('POST')) {
-
             $race = $_POST['race'];
-
             $observations = $this->getDoctrine()->getManager()->getRepository('AppBundle:Observations')->getObservationsByBirdRace($race);
-
             /*$debug = fopen('debug.txt', 'w');
             fwrite($debug, var_export($observations, true));
             fclose($debug);
             die();*/
 
             $response = new JsonResponse();
-
             $lng = [];
             $lat = [];
             $birdName = [];
@@ -58,25 +51,21 @@ class DefaultController extends Controller
             $image = [];
 
             foreach ($observations as $observation) {
-
                 array_push($lng, $observation->getLongitude());
                 array_push($lat, $observation->getLatitude());
                 array_push($birdName, $observation->getBirdName());
                 array_push($date, $observation->getPublishedAt()->format('d/m/Y'));
                 array_push($author, $observation->getAuthor());
                 //array_push($image, $observation->getPictures()[0]->getName());
-
             }
 
             $response->setData(array(
-
                 'longitudes' => $lng,
                 'latitudes'  => $lat,
                 'birdName'   => $birdName,
                 'date'       => $date,
                 'author'     => $author,
                 'image'      => $image,
-
             ));
 
             return $response;
@@ -94,12 +83,6 @@ class DefaultController extends Controller
     public function dashboardAction(Request $request) {
 
         return $this->render('default/dashboard.html.twig');
-
-    }
-
-    public function userListAction(Request $request) {
-
-        return $this->render('default/user_list.html.twig');
 
     }
 
